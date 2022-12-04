0825210803e24ea61432576a923ce90f6c96ea12
Anastasia <Nasti.M@list.ru>
2022-12-05 02:24:25 +0300

    дописать

--- Доделать/Homework_3_4/Program.cs
+++ Доделать/Homework_3_4/Program.cs
@@ -8,22 +8,3 @@ for (int i = 0; i < n; i++) arrayA[i] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координаты точки B: ");
 int [] arrayB = new int[n];
 for (int j = 0; j < n; j++) arrayB[j] = Convert.ToInt32(Console.ReadLine());
-int length = n;
-int a = 0;
-int b = 0;
-int k = 0;
-//string result = String.Empty;
-//if (a < length && b < length )
-//{
-//double result = Math.Round(Math.Sqrt(Math.Pow((k),2) + Math.Pow((arrayA[a]-arrayB[b]),2)),2);
-//k = result;
-//a++;
-//b++;
-//}
-//else
-//{
-  //  Console.WriteLine($"Расстояние между точками {k}");
-//}
-double r = distance (new int[n])
-Console.WriteLine($"Расстояние между точками {r}");
-double distance (int[] arrayA, int[] arrayB)
