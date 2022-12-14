6fa7df151392480f350da8173db0b254476fe07b
Anastasia <Nasti.M@list.ru>
2022-12-14 14:13:29 +0300

    Two

--- Homework_c#_2/Homework_2_2/Program.cs
+++ Homework_c#_2/Homework_2_2/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Enter an integer number!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+// 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.WriteLine("Enter an integer number!");
 int num = Convert.ToInt32(Console.ReadLine());
 int d = 0;
 if (num > 99)
