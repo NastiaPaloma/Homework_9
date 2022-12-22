8d9d50552b234063686ed01800f09eb765d48468
Anastasia <Nasti.M@list.ru>
2022-12-22 19:38:30 +0300

    Seven

---
+++ Homework_c#_7/Homework_7_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание,
+// что такого элемента нет. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+bool CheckNumberIn(int number, int[,] array)
+{
+    bool find = false;
+    foreach (int el in array)
+    {
+        if (el == number)
+        {
+            find = true;
+            break;
+        }
+    }
+    return find;
+}
+
+Console.WriteLine("Введите количетсво строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количетсво столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(CheckNumberIn(number, array));
---
+++ Homework_c#_7/Homework_7_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количетсво строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количетсво столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Sums(array);
+
+void Sums(int[,] array)
+{
+    double[] Arr = new double[cols];
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum = (sum + array[j, i]);
+        }
+        Arr[i] = sum / rows;
+        Console.WriteLine();
+        Console.Write($"Среднее арифметическое столбца: {Math.Round(Arr[i], 1)}\t");
+    }
+}
