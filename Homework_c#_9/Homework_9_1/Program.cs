3c69d37bdaab12895f116e0fbd6f209615dfee1d
Anastasia <Nasti.M@list.ru>
2022-12-31 16:45:28 +0300

    nine

---
+++ Homework_c#_9/Homework_9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"   N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"числа от N до 1: {NumbersRec(n)}");
+
+string NumbersRec(int N)
+{
+    if (N == 0) return " ";
+    return N + " " + NumbersRec(N - 1);
+}
+
---
+++ Homework_c#_9/Homework_9_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120   M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма чисел от {m} до {n} равна {SummaRec(n)}");
+
+int SummaRec(int n)
+{
+    if (m > n) return 0;
+    return n + SummaRec(n - 1);
+}
